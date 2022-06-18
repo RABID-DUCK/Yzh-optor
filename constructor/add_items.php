2286cc21ace655223b326c0d0ea1111b7a1a80ee
RABID-DUCK <54327392+RABID-DUCK@users.noreply.github.com>
2022-06-18 05:00:10 +0500

    complete slider and nav

--- constructor/add_items.php
+++ constructor/add_items.php
@@ -73,11 +73,7 @@
                     mysqli_query($conn, "DELETE FROM `items` WHERE `items`.`id` = '$elem_id'");
                 }
             ?>
-<<<<<<< HEAD
              </tr> </thead>
-=======
-            </tr> </thead>
->>>>>>> daa83c11d053465daa56017b57d1bae5a82beb95
             <?php $elems = mysqli_query($conn, "SELECT * FROM items");
            foreach ($elems as $elem) {
             switch (strlen($elem['id'])) {
@@ -115,13 +111,8 @@
                         <input onclick="del_elem(<?=$elem['id']?>)" type="submit" name="delete" id="bd_del" value="Удалить">
                     </td>
                 </tr>
-<<<<<<< HEAD
             <?php }; ?>
             <script>
-=======
-                <?php }; ?>
-                <script>
->>>>>>> daa83c11d053465daa56017b57d1bae5a82beb95
                     function del_elem(id) {
                         document.getElementById("elem_del_id").value = id;
                     }
